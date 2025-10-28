101f85fb45c199e907428318d1398b4a09d686c8
Thomas Nichols <thomas@nexus10.com>
2025-10-28 20:01:04 +0000

    Set keymappings and begin obsidian.nvim config

--- init.lua
+++ init.lua
@@ -1,39 +1,5 @@
 --[[
 
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
       - https://learnxinyminutes.com/docs/lua/
 
     After understanding a bit more about Lua, you can use `:help lua-guide` as a
@@ -43,15 +9,9 @@ What is Kickstart?
 
 Kickstart Guide:
 
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
       - <enter key>
 
-    (If you already know the Neovim basics, you can skip this step.)
+   (If you already know the Neovim basics, you can skip this step.)
 
   Once you've completed that, you can continue working through **AND READING** the rest
   of the kickstart init.lua.
@@ -105,5 +65,32 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- [[    Misc config    ]]
+
+-- From Perplexity.AI -
+vim.g.netrw_sort_by = 'time'
+vim.g.netrw_sort_direction = 'reverse'
+
+vim.keymap.set('i', 'jj', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+
+-- Normal mode: move current line
+vim.keymap.set('n', '<C-Up>', ':m .-2<CR>==', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-Down>', ':m .+1<CR>==', { noremap = true, silent = true })
+
+-- Insert mode: move current line and resume insert
+vim.keymap.set('i', '<C-Up>', '<Esc>:m .-2<CR>==gi', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-Down>', '<Esc>:m .+1<CR>==gi', { noremap = true, silent = true })
+
+-- Visual mode: move selection
+vim.keymap.set('v', '<C-Up>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set('v', '<C-Down>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+
+-- :set guifont=Monospace:h14
+-- vim.opt.guifont = 'Iosevka NF Medium:h14'
+-- vim.opt.guifont = 'Consolas:h10'
+
+--
+--
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -12,6 +12,37 @@
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  -- Obsidian syntax support
+  --[[ 
+ -- {
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    ft = 'markdown',
+    Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+      -- refer to `:h file-pattern` for more examples
+      "BufReadPre path/to/my-vault/*.md",
+      "BufNewFile path/to/my-vault/*.md",
+    },
+    -@module 'obsidian'
+    -- -@type obsidian.config
+    opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '~/vaults/personal',
+        },
+        {
+          name = 'work',
+          path = '~/Documents/obsidian-pigsty',
+        },
+      },
+    },
+
+  },
+  --]]
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
